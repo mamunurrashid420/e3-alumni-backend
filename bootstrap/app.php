5a1f91f4028f7cd7c2c58f7c71a8ced202b2080b
Mehedi Hasan Tushar <meheditushar.cs@gmail.com>
2026-02-02 23:09:33 +0600

    rate limit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(prepend: ['throttle:api']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // Return JSON responses for API routes when authentication fails
